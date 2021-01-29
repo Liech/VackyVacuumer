1c3544ddf77c751041eed681087810c171cca510
Liech <nicolas_harttig@hotmail.de>
2021-01-29 21:12:46 +0100

    made changes on wasd controler

--- Vacky/Assets/Source/WASD.cs
+++ Vacky/Assets/Source/WASD.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class WASD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public float Speed;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-    if (Input.GetKey(KeyCode.W))
-      transform.position += new Vector3(0, 0.1f, 0);
-    if (Input.GetKey(KeyCode.S))
-      transform.position += new Vector3(0, -0.1f, 0);
-    if (Input.GetKey(KeyCode.A))
-      transform.position += new Vector3(-0.1f, 0, 0);
-    if (Input.GetKey(KeyCode.D))
-      transform.position += new Vector3(0.1f, 0, 0);
+  private bool _isStunned = false;
 
+  // Start is called before the first frame update
+  void Start()
+  {
+      
+  }
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    if (_isStunned)
+      return;
+
+    bool w = Input.GetKey(KeyCode.W);
+    bool a = Input.GetKey(KeyCode.A);
+    bool s = Input.GetKey(KeyCode.S);
+    bool d = Input.GetKey(KeyCode.D);
+
+    float xSpeed = (a ? -1.0f : 0) + (d ? 1.0f : 0);
+    float ySpeed = (s ? -1.0f : 0) + (w ? 1.0f : 0);
+
+    //transform.GetComponent<Rigidbody2D>().angularVelocity = 
+    transform.GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed,ySpeed);
+  }
+
+  public void stun(float seconds)
+  {
+    StopAllCoroutines();
+    _isStunned = true;
+    StartCoroutine("stopStun");
+  }
+
+  private IEnumerator stopStun(float seconds)
+  {
+    yield return new WaitForSeconds(seconds);
+    _isStunned = false;
   }
 }
