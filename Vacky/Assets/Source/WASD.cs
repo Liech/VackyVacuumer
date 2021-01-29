8252a24b5c35fc6662c68b0cbf0a144778341dc7
Liech <nicolas_harttig@hotmail.de>
2021-01-29 21:37:37 +0100

    robo can turn and forwaaaaaard

--- Vacky/Assets/Source/WASD.cs
+++ Vacky/Assets/Source/WASD.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class WASD : MonoBehaviour
 {
-  public float Speed;
+  public float Speed = 1.0f;
+  public float RotationSpeed = 1.0f;
+  public float BackwardsDrive = 1.0f;
 
   private bool _isStunned = false;
 
@@ -25,11 +27,12 @@ public class WASD : MonoBehaviour
     bool s = Input.GetKey(KeyCode.S);
     bool d = Input.GetKey(KeyCode.D);
 
-    float xSpeed = (a ? -1.0f : 0) + (d ? 1.0f : 0);
-    float ySpeed = (s ? -1.0f : 0) + (w ? 1.0f : 0);
+    float xSpeed = -((a ? -1.0f : 0) * BackwardsDrive + (d ? 1.0f : 0)) * RotationSpeed;
+    float ySpeed = (s ? -1.0f : 0) + (w ? 1.0f : 0)*Speed;
 
-    //transform.GetComponent<Rigidbody2D>().angularVelocity = 
-    transform.GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed,ySpeed);
+    transform.GetComponent<Rigidbody2D>().angularVelocity = xSpeed;
+    float angle = Mathf.PI * transform.GetComponent<Rigidbody2D>().rotation / 180.0f;
+    transform.GetComponent<Rigidbody2D>().velocity = new Vector2( Mathf.Sin(-angle) * ySpeed, Mathf.Cos(-angle) * ySpeed);
   }
 
   public void stun(float seconds)
