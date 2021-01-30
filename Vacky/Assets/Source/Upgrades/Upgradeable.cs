52452da8be7b08dcd55038c3c0dd581385d46f41
Liech <nicolas_harttig@hotmail.de>
2021-01-30 22:47:19 +0100

    added mop and imprved death

--- Vacky/Assets/Source/Upgrades/Upgradeable.cs
+++ Vacky/Assets/Source/Upgrades/Upgradeable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Upgradeable : MonoBehaviour
+public class Upgradeable : IController
 {
   private List<Upgrader> upgrader;
   private Upgrader currentUpgrader;
