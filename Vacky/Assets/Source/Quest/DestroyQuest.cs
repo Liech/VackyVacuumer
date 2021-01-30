2ff33ff821fc7f248d06218034571d76b6350268
DerKinderRiegel <derkinderriegel@googlemail.com>
2021-01-30 21:02:44 +0100

    added door object, and used it in maschinenraum

---
+++ Vacky/Assets/Source/Quest/DestroyQuest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyQuest : Quest
+{
+  public override void questDone(string ID)
+  {
+    Debug.Log("Now I will open");
+    //Instantiate(toSpawn, transform.position, Quaternion.identity);
+    Destroy(gameObject);
+  }
+}
