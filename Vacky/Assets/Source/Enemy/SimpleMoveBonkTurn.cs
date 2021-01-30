fe76fe46cf94c086c4014d8fb5b8b33bc3bb7101
theWuukie <78221645+theWuukie@users.noreply.github.com>
2021-01-30 22:46:21 +0100

    wollmaus upgraaade

--- Vacky/Assets/Source/Enemy/SimpleMoveBonkTurn.cs
+++ Vacky/Assets/Source/Enemy/SimpleMoveBonkTurn.cs
@@ -7,10 +7,11 @@ public class SimpleMoveBonkTurn : IController, IStunnable
   public Vector2 currentDirection = new Vector2(1, 0);
   public float MoveSpeed = 1;
   private bool _isStunned = false;
+  
 
   void Start()
   {
-    
+    currentDirection.Normalize();
   }
 
   // Update is called once per frame
@@ -18,12 +19,18 @@ public class SimpleMoveBonkTurn : IController, IStunnable
   {
     if (_isStunned)
       return;
-    GetComponent<Rigidbody2D>().velocity = currentDirection * MoveSpeed;
+    Vector2 vel = currentDirection * MoveSpeed;
+    GetComponent<Rigidbody2D>().velocity = vel;
+    GetComponent<Rigidbody2D>().rotation = Mathf.Atan2(vel.y, vel.x) * Mathf.Rad2Deg - 90;
   }
 
   void OnCollisionEnter2D(Collision2D collision)
   {
     currentDirection *= -1;
+    Vector3 s = transform.localScale;
+    s.x *= -1;
+    transform.localScale = s;
+
   }
 
   public void stun(float seconds)
